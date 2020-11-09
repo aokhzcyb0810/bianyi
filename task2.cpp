c38551e224e9943cc9cebd1f35fe6b75f9f96a5c
aokhzcyb0810 <63200182+aokhzcyb0810@users.noreply.github.com>
2020-11-09 15:37:03 +0800

    Add files via upload

---
+++ task2.cpp
@@ -0,0 +1,182 @@
+#include<iostream>
+#include<stdlib.h>
+#include<string.h>
+char stack[10000],stackq[10000];
+int f(char a,char b);
+int main(int argc,char* argv[]) {
+	FILE *fp;
+	fp=fopen(argv[1],"r");
+	char a,c;
+	int p=-1,q=-1;
+	stack[++p]='a';
+	while(fscanf(fp,"%c",&a)!=EOF){
+		if(a=='\n' || a=='\r')
+		break;
+		else{
+				if(f(stack[p],a)==0){
+					stack[++p]=a;
+					printf("I%c\n",a);
+				}
+				else if(f(stack[p],a)==1){
+					while(f(stack[p],a)==1){
+						if(stack[p]=='i'){
+							stackq[++q]='i';
+							p--;
+							printf("R\n");
+						}
+						else{
+							if(q>0){
+								p--;
+								q--;
+								printf("R\n");
+							}
+							else{
+								printf("RE\n");
+								return 0;
+							}
+						}
+					}
+					stack[++p]=a;
+					printf("I%c\n",a);
+					if(f(stack[p-1],stack[p])==2){
+						printf("R\n");
+						p-=2;
+					}
+				}
+				else if(f(stack[p],a)==-1){
+					printf("E\n");
+					return 0;
+				}
+		}
+		}
+		a='a';
+		while(stack[p]!='a'){
+		if(f(stack[p],a)==0){
+					stack[++p]=a;
+				}
+				else if(f(stack[p],a)==1){
+					while(f(stack[p],a)==1){
+						if(stack[p]=='i'){
+							stackq[++q]='i';
+							p--;
+							printf("R\n");
+						}
+						else{
+							if(q>0){
+								p--;
+								q--;
+								printf("R\n");
+							}
+							else{
+								printf("RE\n");
+								return 0;
+							}
+						}
+					}
+					stack[++p]=a;
+				//	printf("I%c\n",a);
+				if(f(stack[p-1],stack[p])==2){
+						printf("R\n");
+						p-=2;
+					}
+				}
+				else if(f(stack[p],a)==-1){
+					printf("E\n");
+					return 0;
+				}
+			}
+	return 0;
+}
+int f(char a,char b){
+	if(a=='a'){
+		if(b=='+')
+		return 0;
+		if(b=='*')
+		return 0;
+		if(b=='i')
+		return 0;
+		if(b=='(')
+		return 0;
+		if(b==')')
+		return -1;
+		if(b=='a')
+		return -1;
+		return -1;
+	}
+	if(a=='+'){
+		if(b=='+')
+		return 1;
+		if(b=='*')
+		return 0;
+		if(b=='i')
+		return 0;
+		if(b=='(')
+		return 0;
+		if(b==')')
+		return 1;
+		if(b=='a')
+		return 1;
+		return -1;
+	}
+	if(a=='*'){
+		if(b=='+')
+		return 1;
+		if(b=='*')
+		return 1;
+		if(b=='i')
+		return 0;
+		if(b=='(')
+		return 0;
+		if(b==')')
+		return 1;
+		if(b=='a')
+		return 1;
+		return -1;
+	}
+	if(a=='i'){
+		if(b=='+')
+		return 1;
+		if(b=='*')
+		return 1;
+		if(b=='i')
+		return -1;
+		if(b=='(')
+		return -1;
+		if(b==')')
+		return 1;
+		if(b=='a')
+		return 1;
+		return -1;
+	}
+	if(a=='('){
+		if(b=='+')
+		return 0;
+		if(b=='*')
+		return 0;
+		if(b=='i')
+		return 0;
+		if(b=='(')
+		return 0;
+		if(b==')')
+		return 2;
+		if(b=='a')
+		return -1;
+		return -1;
+	}
+	if(a==')'){
+		if(b=='+')
+		return 1;
+		if(b=='*')
+		return 1;
+		if(b=='i')
+		return -1;
+		if(b=='(')
+		return -1;
+		if(b==')')
+		return 1;
+		if(b=='a')
+		return 1;
+		return -1;
+	}
+	
+}
